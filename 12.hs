972285ab57bb4e2ad1dc20f279b41862bb727145
chisun-joung <csjoung@gmail.com>
2014-12-26 22:45:24 +0900

    12

---
+++ 12.hs
@@ -0,0 +1,32 @@
+
+last' :: [a] -> a
+last' [x] = x
+last' (_ : xs) = last xs
+
+foldr' :: (a -> b -> b) -> b -> [a] -> b
+foldr' f v ( x : xs ) = f x (foldr f v xs)
+foldr' _ v [] = v
+
+init' :: [a] -> [a]
+init' [_] = []
+init' (x : xs) = x : init xs
+
+
+drop' :: Int -> [a] -> [a]
+drop' n [] = []
+drop' 0 xs = xs 
+drop' n (_ : xs) = drop (n - 1) xs 
+
+(++!) :: [a] -> [a] -> [a]
+(x : xs) ++! ys = x : (xs ++! ys)
+[] ++! ys = ys
+
+foldl' :: (a ->b ->a) -> a -> [b] -> a
+foldl' f v (x : xs) = foldl f (f v x) xs
+foldl' _ v [] = v
+
+data Nat = Zero | Succ Nat
+
+add :: Nat -> Nat -> Nat
+add Zero m = m
+add (Succ n) m = Succ (add n m)
